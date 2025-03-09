024ec39e8f793495b37c9a35f406a37810a92d2e
ruihong123 <wang4996@purdue.edu>
2025-03-08 22:39:37 -0500

    debug

--- scripts/db_TPCC_exp.sh
+++ scripts/db_TPCC_exp.sh
@@ -136,7 +136,7 @@ vary_read_ratios () {
 vary_query_ratio () {
   #read_ratios=(0 30 50 70 90 100)
   thread_number=(8)
-  WarehouseNum=(64)
+  WarehouseNum=(16)
   FREQUENCY_DELIVERY=(100 0 0 0 0 1)
   FREQUENCY_PAYMENT=(0 100 0 0 0 10)
   FREQUENCY_NEW_ORDER=(0 0 100 0 0 10)
