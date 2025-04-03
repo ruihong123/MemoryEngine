1708759fc981afeb9cf812b45cb39065cf72193c
ruihong123 <wang4996@purdue.edu>
2025-04-03 13:24:36 -0400

    solve the problem of receive MRs out of buffers.

--- txn/TxnManagerTo.cpp
+++ txn/TxnManagerTo.cpp
@@ -375,7 +375,7 @@ namespace DSMEngine{
             throw std::runtime_error("There are still some latches hold in the transaction commit.");
             assert(false);
         }
-        ClearAllLatches();
+//        ClearAllLatches();
         is_first_access_ = true;
         PROFILE_TIME_END(thread_id_, CC_COMMIT);
         return true;
