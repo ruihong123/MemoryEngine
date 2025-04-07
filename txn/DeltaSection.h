5670f163573dd7738f0797bbc526bb8ed2b79f4b
ruihong123 <wang4996@purdue.edu>
2025-04-07 19:57:07 -0400

    solve the problem of receive MRs out of buffers.

--- txn/DeltaSection.h
+++ txn/DeltaSection.h
@@ -314,7 +314,7 @@ namespace DSMEngine {
         void CalculateWriteBoundaries(std::vector<std::pair<size_t, size_t>> & boundaries, uint64_t old_h, uint64_t old_t, uint64_t old_epoch){
             //todo: the logic need carefully proofread.
             while(inner_section->tail_ != inner_section->tail_allocated){
-                assert(inner_section->tail_ < inner_section->tail_allocated);
+                assert(inner_section->tail_ <= inner_section->tail_allocated);
                 // no ops
                 _mm_pause();
             }
