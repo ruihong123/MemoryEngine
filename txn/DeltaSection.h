cefc51a48678b82f83e25282667bb6e524a5316b
ruihong123 <wang4996@purdue.edu>
2025-04-09 20:25:25 -0400

    solve the problem of receive MRs out of buffers.

--- txn/DeltaSection.h
+++ txn/DeltaSection.h
@@ -112,35 +112,39 @@ namespace DSMEngine {
             //todo the max_ts need to be guarded by a mtx.
             MetaColumn meta_col = old_record->GetMeta();
             // update the max time stamp.
-            if (inner_section->max_ts < meta_col.Wts_){
+            if (inner_section->max_ts < meta_col.Wts_) {
                 inner_section->max_ts = meta_col.Wts_;
             }
-            DeltaRecord * delta_record = new(inner_section->local_seg_addr_ + offset_to_write) DeltaRecord(
+            DeltaRecord *delta_record = new(inner_section->local_seg_addr_ + offset_to_write) DeltaRecord(
                     meta_col.Wts_, delta_size, meta_col.prev_delta_, commit_ts,
-                    meta_col.prev_delta_epoch_, meta_col.prev_delta_data_size_ );
+                    meta_col.prev_delta_epoch_, meta_col.prev_delta_data_size_);
             old_record->dirty_col_ids = std::move(new_record->dirty_col_ids);
             old_record->serialize_to_delta(delta_record);
 #ifndef NDEBUG
-            if (next_offset < prev_offset && prev_offset < seg_real_size_){
-                assert(*((char*)(inner_section->local_seg_addr_ + prev_offset)) == '^');
+            if (next_offset < prev_offset && prev_offset < seg_real_size_) {
+                assert(*((char *) (inner_section->local_seg_addr_ + prev_offset)) == '^');
             }
 #endif
 
-            assert((char*)delta_record + delta_size <= (char*)seg_local_mr_->addr + seg_local_mr_->length);
+            assert((char *) delta_record + delta_size <= (char *) seg_local_mr_->addr + seg_local_mr_->length);
             delta_gadd = seg_addr_;
             delta_gadd.offset += offset_to_write + STRUCT_OFFSET(DeltaSection, local_seg_addr_);
-            uint64_t epoch_before = inner_section->epoch;
-            uint64_t  tail_shot_before = inner_section->tail_;
-            while(!inner_section->tail_.compare_exchange_strong(prev_offset, next_offset, std::memory_order_seq_cst,
-                                                             std::memory_order_seq_cst)){
-                _mm_pause();
-            };
-            printf("Node %d thread %d has modified tail_ from %lu to %lu\n", rdma_mg_->node_id, rdma_mg_->thread_id, prev_offset, next_offset);
-            fflush(stdout);
-            uint64_t epoch = inner_section->epoch;
-            uint64_t  tail_shot = inner_section->tail_;
+            {
+                std::unique_lock<std::shared_mutex> lck(ds_mtx_);
+                uint64_t epoch_before = inner_section->epoch;
+                uint64_t tail_shot_before = inner_section->tail_;
+                while (!inner_section->tail_.compare_exchange_strong(prev_offset, next_offset, std::memory_order_seq_cst,
+                                                                     std::memory_order_seq_cst)) {
+                    _mm_pause();
+                };
+                printf("Node %d thread %d has modified tail_ from %lu to %lu\n", rdma_mg_->node_id, rdma_mg_->thread_id,
+                       prev_offset, next_offset);
+                fflush(stdout);
+                uint64_t epoch = inner_section->epoch;
+                uint64_t tail_shot = inner_section->tail_;
 
-            assert(tail_shot > prev_offset || prev_offset > next_offset);
+                assert(tail_shot > prev_offset || prev_offset > next_offset);
+            }
 
         }
 #endif
