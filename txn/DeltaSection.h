2f951a76de80878210cfe0a21d7f5e708a6400ca
ruihong123 <wang4996@purdue.edu>
2025-04-17 22:50:09 -0400

    debug for lock free delta section write buffer.

--- txn/DeltaSection.h
+++ txn/DeltaSection.h
@@ -96,8 +96,8 @@ namespace DSMEngine {
                 inner_section->is_empty_ = false;
             }
             next_offset = inner_section->tail_allocated;
-//            printf("Node %d thread %d modify tail_ from %lu to %lu\n", rdma_mg_->node_id, rdma_mg_->thread_id, prev_offset, next_offset);
-//            fflush(stdout);
+            printf("Node %d thread %d modify tail_ from %lu to %lu\n", rdma_mg_->node_id, rdma_mg_->thread_id, prev_offset, next_offset);
+            fflush(stdout);
             return return_offset;
 
         }
