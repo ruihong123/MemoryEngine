96253509527419daf54f9532bda2b60cbcb0068d
ruihong123 <wang4996@purdue.edu>
2025-07-07 02:22:14 +0000

    Change TPCC generator to use DynamicCompoundKey.

--- tpcc/TpccKeyGenerator.h
+++ tpcc/TpccKeyGenerator.h
@@ -24,83 +24,132 @@ static DynamicCompoundKey* GetItemPrimaryKey(int i_id, int w_id) {
 //  IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
 //      | (((IndexKey) ITEM_TABLE_ID) << kTableIdLowBits);
 //  k = k | i_id;
-DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  //TODO: Think about how and when to deallocate the buffer.
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
   return ret;
 }
-static IndexKey GetWarehousePrimaryKey(int w_id) {
+static DynamicCompoundKey* GetWarehousePrimaryKey(int w_id) {
   assert(w_id >= 1 && w_id <= tpcc_scale_params.num_warehouses_);
-  IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+  char* prim_buffer = new char[sizeof(int)];
+  //copy the value into the buffer
+    memcpy(prim_buffer, &w_id, sizeof(int));
+  // IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
 //      | (((IndexKey) WAREHOUSE_TABLE_ID) << kTableIdLowBits);
-  return k;
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
-static IndexKey GetDistrictPrimaryKey(int d_id, int d_w_id) {
+static DynamicCompoundKey* GetDistrictPrimaryKey(int d_id, int d_w_id) {
   assert(
       d_id >= 1 && d_id <= tpcc_scale_params.num_districts_per_warehouse_
           && d_w_id >= 1 && d_w_id <= tpcc_scale_params.num_warehouses_);
-  IndexKey k = (((IndexKey) d_w_id) << kWarehouseBits);
-//      | (((IndexKey) DISTRICT_TABLE_ID) << kTableIdLowBits);
-  k = k | (((IndexKey) d_id) << kDistrictLowBits);
-  return k;
+      char* prim_buffer = new char[sizeof(int) + sizeof(int)];
+    memcpy(prim_buffer, &d_id, sizeof(int));
+    memcpy(prim_buffer + sizeof(int), &d_w_id, sizeof(int));
+//   IndexKey k = (((IndexKey) d_w_id) << kWarehouseBits);
+// //      | (((IndexKey) DISTRICT_TABLE_ID) << kTableIdLowBits);
+//   k = k | (((IndexKey) d_id) << kDistrictLowBits);
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
-static IndexKey GetCustomerPrimaryKey(int c_id, int c_d_id, int c_w_id) {
+static DynamicCompoundKey* GetCustomerPrimaryKey(int c_id, int c_d_id, int c_w_id) {
   assert(c_id >= 1 && c_id <= tpcc_scale_params.num_customers_per_district_);
-  IndexKey k = (((IndexKey) c_w_id) << kWarehouseBits);
+  char* prim_buffer = new char[sizeof(int) + sizeof(int) + sizeof(int)];
+  memcpy(prim_buffer, &c_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int), &c_d_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int) + sizeof(int), &c_w_id, sizeof(int));
+  // IndexKey k = (((IndexKey) c_w_id) << kWarehouseBits);
 //      | (((IndexKey) CUSTOMER_TABLE_ID) << kTableIdLowBits);
-  k = k | (((IndexKey) c_d_id) << kDistrictLowBits);
-  k = k | c_id;
-  return k;
+  // k = k | (((IndexKey) c_d_id) << kDistrictLowBi/ts);
+  // k = k | c_id;
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
 //TODO: a more feasible encoder for order_key
-static IndexKey GetOrderPrimaryKey(int o_id, int o_d_id, int o_w_id) {
-  IndexKey k = (((IndexKey) o_w_id) << kWarehouseBits);
+static DynamicCompoundKey* GetOrderPrimaryKey(int o_id, int o_d_id, int o_w_id) {
+  // IndexKey k = (((IndexKey) o_w_id) << kWarehouseBits);
+  char* prim_buffer = new char[sizeof(int) + sizeof(int) + sizeof(int)];
+  memcpy(prim_buffer, &o_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int), &o_d_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int) + sizeof(int), &o_w_id, sizeof(int));
 //      | (((IndexKey) ORDER_TABLE_ID) << kTableIdLowBits);
-  k = k | (((IndexKey) o_d_id) << kDistrictLowBits);
-  k = k | (((IndexKey) o_id) << kOrderIdLowBits);
-  return k;
+  // k = k | (((IndexKey) o_d_id) << kDistrictLowBits);
+  // k = k | (((IndexKey) o_id) << kOrderIdLowBits);
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
-static IndexKey GetDistrictNewOrderPrimaryKey(int d_id, int w_id) {
-  IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+static DynamicCompoundKey* GetDistrictNewOrderPrimaryKey(int d_id, int w_id) {
+  char* prim_buffer = new char[sizeof(int) + sizeof(int)];
+  memcpy(prim_buffer, &d_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int), &w_id, sizeof(int));
+  // IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
 //      | (((IndexKey) DISTRICT_NEW_ORDER_TABLE_ID) << kTableIdLowBits);
-  k = k | (((IndexKey) d_id) << kDistrictLowBits);
-  return k;
+  // k = k | (((IndexKey) d_id) << kDistrictLowBits);
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
-static IndexKey GetNewOrderPrimaryKey(int o_id, int d_id, int w_id) {
-  IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+static DynamicCompoundKey* GetNewOrderPrimaryKey(int o_id, int d_id, int w_id) {
+  // IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+  char* prim_buffer = new char[sizeof(int) + sizeof(int) + sizeof(int)];
+  memcpy(prim_buffer, &o_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int), &d_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int) + sizeof(int), &w_id, sizeof(int));
 //      | (((IndexKey) NEW_ORDER_TABLE_ID) << kTableIdLowBits);
-  k = k | (((IndexKey) d_id) << kDistrictLowBits);
-  k = k | (((IndexKey) o_id) << kOrderIdLowBits);
-  return k;
+  // k = k | (((IndexKey) d_id) << kDistrictLowBits);
+  // k = k | (((IndexKey) o_id) << kOrderIdLowBits);
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
-static IndexKey GetOrderLinePrimaryKey(int o_id, int d_id, int w_id,
+static DynamicCompoundKey* GetOrderLinePrimaryKey(int o_id, int d_id, int w_id,
                                        int ol_no) {
-  IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+  // IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+  char* prim_buffer = new char[sizeof(int) + sizeof(int) + sizeof(int) + sizeof(int)];
+  memcpy(prim_buffer, &o_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int), &d_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int) + sizeof(int), &w_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int) + sizeof(int) + sizeof(int), &ol_no, sizeof(int));
 //      | (((IndexKey) ORDER_LINE_TABLE_ID) << kTableIdLowBits);
-  k = k | (((IndexKey) d_id) << kDistrictLowBits);
-  k = k | (((IndexKey) o_id) << kOrderIdLowBits);
-  k = k | ol_no;
-  return k;
+  // k = k | (((IndexKey) d_id) << kDistrictLowBits);
+  // k = k | (((IndexKey) o_id) << kOrderIdLowBits);
+  // k = k | ol_no;
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
-static IndexKey GetHistoryPrimaryKey(int c_id, int d_id, int w_id) {
+static DynamicCompoundKey* GetHistoryPrimaryKey(int c_id, int d_id, int w_id) {
   // as new HISTORY record inserted, history record may not be unique
   // anymore
-  IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+  // IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+  char* prim_buffer = new char[sizeof(int) + sizeof(int) + sizeof(int)];
+  memcpy(prim_buffer, &c_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int), &d_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int) + sizeof(int), &w_id, sizeof(int));
 //      | (((IndexKey) HISTORY_TABLE_ID) << kTableIdLowBits);
-  k = k | (((IndexKey) d_id) << kDistrictLowBits);
-  k = k | c_id;
-  return k;
+  // k = k | (((IndexKey) d_id) << kDistrictLowBits);
+  // k = k | c_id;
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
-static IndexKey GetStockPrimaryKey(int i_id, int w_id) {
-  IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+static DynamicCompoundKey* GetStockPrimaryKey(int i_id, int w_id) {
+  // IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+  char* prim_buffer = new char[sizeof(int) + sizeof(int)];
+  memcpy(prim_buffer, &i_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int), &w_id, sizeof(int));
 //      | (((IndexKey) STOCK_TABLE_ID) << kTableIdLowBits);
-  k = k | i_id;
-  return k;
+  // k = k | i_id;
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
-static IndexKey GetOrderLineSecondaryKey(int o_id, int d_id, int w_id) {
-  IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+// TODO: check if this is correct
+static DynamicCompoundKey* GetOrderLineSecondaryKey(int o_id, int d_id, int w_id) {
+  // IndexKey k = (((IndexKey) w_id) << kWarehouseBits);
+  char* prim_buffer = new char[sizeof(int) + sizeof(int) + sizeof(int)];
+  memcpy(prim_buffer, &o_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int), &d_id, sizeof(int));
+  memcpy(prim_buffer + sizeof(int) + sizeof(int), &w_id, sizeof(int));
 //      | (((IndexKey) ORDER_LINE_TABLE_ID) << kTableIdLowBits);
-  k = k | (((IndexKey) d_id) << kDistrictLowBits);
-  k = k | (((IndexKey) o_id) << kOrderIdLowBits);
-  return k;
+  // k = k | (((IndexKey) d_id) << kDistrictLowBits);
+  // k = k | (((IndexKey) o_id) << kOrderIdLowBits);
+  DynamicCompoundKey* ret = reinterpret_cast<DynamicCompoundKey*>(prim_buffer);
+  return ret;
 }
 
 }
